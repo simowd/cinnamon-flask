ad940ae019e0bfde3a6104e18076c61c07ed0c9f
Martín Campuzano <41243969+simowd@users.noreply.github.com>
2020-12-10 14:16:54 -0400

    Update __init__.py

--- cinnamon/__init__.py
+++ cinnamon/__init__.py
@@ -8,7 +8,7 @@ from flask_security import SQLAlchemyUserDatastore, Security
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zylzojixmwbqsg:1ade1d6715be01d74e0ea91cced1778324ad5bef35016a1103d5adb01073c561@ec2-34-237-166-54.compute-1.amazonaws.com:5432/db8od3ulqain3f'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECURITY_USER_IDENTITY_ATTRIBUTES'] = 'username'    
     app.config['SECRET_KEY'] = 'huachimingosal100'
