5fb839346892e7f107f4d218cef2437497655023
Martin Campuzano <martin.campuzano.flores@gmail.com>
2020-12-10 13:53:42 -0400

    Added login security and made the login, logout functions

--- cinnamon/api.py
+++ cinnamon/api.py
@@ -1,12 +1,42 @@
-from flask_restful import Resource, Api
-from .models import db, User
+from flask_restful import Resource, Api, reqparse
+from .models import db, User, Role
+from flask_security import login_required, current_user, login_user, auth_token_required,SQLAlchemyUserDatastore, Security, utils
+from flask import jsonify
 
 api = Api()
 
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+
+parser = reqparse.RequestParser()
+parser.add_argument('username')
+parser.add_argument('password')
+
+class Auth(Resource):
+    def post(self):
+        args = parser.parse_args()
+        user = user_datastore.get_user(args['username'])
+        if user != None:
+            if utils.verify_password(args['password'], user.password):
+                login_user(user)
+                token = user.get_auth_token()
+                return {'Authorization-Token' : token}
+            else:
+                return {'message':'Contraseña incorrecta'}, 401
+        else:
+            return {'message':'El usuario no existe'}, 401
+        return "Sucedió algún error"
+    def delete(self):
+        utils.logout_user();
+        return {'message': 'Log-out exitoso'}, 200
+
 class HelloWorld(Resource):
     def get(self):
+        print(current_user)
+        if not current_user.is_authenticated:
+            return {'message':'Error en la autorización'}, 403
         users = User.query.all()
-        print(users)
+        print(current_user)
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(Auth,'/log-in')
--- cinnamon/models.py
+++ cinnamon/models.py
@@ -1,12 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import UserMixin, RoleMixin
  
 db = SQLAlchemy()
 
-class User(db.Model):
-    id_user = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(50), nullable = False)
+class User(db.Model, UserMixin):
+    id = db.Column('id_user', db.Integer, primary_key = True)
+    username = db.Column(db.String(50), nullable = False, unique = True)
+    email = db.Column(db.String(50), nullable = True, unique = True)
+    id_role = db.Column(db.Integer, db.ForeignKey('role.id_role'), nullable = False)
     password = db.Column(db.String(45), nullable = False)
     name = db.Column(db.String(40), nullable = True)
     lastname = db.Column(db.String(45), nullable = True)
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Role(db.Model, RoleMixin):
+    id = db.Column('id_role', db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False)
+    description = db.Column(db.String(255), nullable = False)
+    users = db.relationship('User', lazy = True)
+    def __repr__(self):
+        return '<Role %r>' % self.name
