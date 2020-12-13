06e69f33a50c9926aa59901000adaa588afabcde
Martin Campuzano <martin.campuzano.flores@gmail.com>
2020-12-13 11:13:57 -0400

    Fixed init py

--- cinnamon/__init__.py
+++ cinnamon/__init__.py
@@ -7,7 +7,6 @@ from flask_security import SQLAlchemyUserDatastore, Security
 from flask_cors import CORS, cross_origin
 
 
-
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     cors = CORS()
@@ -25,14 +24,13 @@ def create_app(test_config=None):
     def creds(response):
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         return response
-    
-    #Initialize flask modules (Rest and SQLAlchemy)
+
+    # Initialize flask modules (Rest and SQLAlchemy)
     db.init_app(app)
     api.init_app(app)
-    cors.init_app(app, supports_credentials=True, origins='http://localhost:8080')
+    cors.init_app(app, supports_credentials=True,
+                  origins='http://localhost:8080')
 
     security = Security(app, user_datastore)
-    
-    
 
     return app
